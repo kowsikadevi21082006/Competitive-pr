aa16b20d6e30c1e6cced67a9aab9b37c2bd0d5c6
kowsikadevi21082006 <deva36rp@gamil.com>
2024-08-17 08:25:43 +0530

    Week 3 Day 5 is Completed

---
+++ week-3/day-5/BoatsSave.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numRescueBoats(self, people: list[int], limit: int) -> int:
+        people.sort()
+        boats = l = 0
+        r = len(people) - 1      
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r, boats = r - 1, boats + 1
+        return boats
+        
